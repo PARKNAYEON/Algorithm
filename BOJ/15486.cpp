7f0e798d559459c3e00a32544c6765c3255df800
PARKNAYEON <parkna96@gmail.com>
2021-01-06 00:05:15 +0900

    update timeover

--- BOJ/15486.cpp
+++ BOJ/15486.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int T[51];
-int P[1001];
-int dp[1001];
+int T[1500005];
+int P[1500005];
+int dp[150005];
 int N;
 int main() {
 
@@ -12,29 +12,37 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		cin >> T[i];
 		cin >> P[i];
+		//dp[i] = P[i];
 	}
-	
+
 	for (int i = 1; i <= N; i++) {
+		int a = 0;
 		for (int j = 1; j < i; j++) {
 			if (i - j >= T[j]) {
-				if(T[i] <= N - i){
-					dp[i] = 0;
+				if (T[i] > N - i + 1) {
+					P[i] = 0;
+					continue;
 				}
-				dp[i] = P[i] + P[j];
-				cout << i <<","<< dp[i] <<","<< P[j];
+
+				a = max(a, P[j]);
+
+				//cout << i <<","<< P[i] <<","<< P[j]<< endl;
 				//cout << "a" << a << endl;
 				//dp[i] = max(a, dp[i]);
-				
+
 			}
 		}
+		P[i] = a + P[i];
 	}
-	
+
 	for (int i = 1; i <= N; i++) {
-		cout << dp[i] << endl;
-		if (maxSum < dp[i])
-			maxSum = dp[i];
+		//cout << P[i] << endl;
+		if (maxSum < P[i])
+			maxSum = P[i];
 	}
 
 	cout << maxSum;
 
 }
+
+//time over
