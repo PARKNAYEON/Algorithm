62f822f0bc4af132969fb49263577bb4bab495a1
PARKNAYEON <parkna96@gmail.com>
2021-01-06 15:41:50 +0900

    update 15486

--- BOJ/15486.cpp
+++ BOJ/15486.cpp
@@ -1,3 +1,54 @@
+//first - time over
+
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int T[1500005];
+//int P[1500005];
+//int dp[150005];
+//int N;
+//int main() {
+//
+//	int maxSum = 0;
+//	cin >> N;
+//	for (int i = 1; i <= N; i++) {
+//		cin >> T[i];
+//		cin >> P[i];
+//		//dp[i] = P[i];
+//	}
+//
+//	for (int i = 1; i <= N; i++) {
+//		int a = 0;
+//		for (int j = 1; j < i; j++) {
+//			if (i - j >= T[j]) {
+//				if (T[i] > N - i + 1) {
+//					P[i] = 0;
+//					continue;
+//				}
+//
+//				a = max(a, P[j]);
+//
+//				//cout << i <<","<< P[i] <<","<< P[j]<< endl;
+//				//cout << "a" << a << endl;
+//				//dp[i] = max(a, dp[i]);
+//
+//			}
+//		}
+//		P[i] = a + P[i];
+//	}
+//
+//	for (int i = 1; i <= N; i++) {
+//		//cout << P[i] << endl;
+//		if (maxSum < P[i])
+//			maxSum = P[i];
+//	}
+//
+//	cout << maxSum;
+//
+//}
+
+
+//success
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -15,34 +66,21 @@ int main() {
 		//dp[i] = P[i];
 	}
 
-	for (int i = 1; i <= N; i++) {
-		int a = 0;
-		for (int j = 1; j < i; j++) {
-			if (i - j >= T[j]) {
-				if (T[i] > N - i + 1) {
-					P[i] = 0;
-					continue;
-				}
+	for (int i = 1; i <= N + 1; i++) {
 
-				a = max(a, P[j]);
-
-				//cout << i <<","<< P[i] <<","<< P[j]<< endl;
-				//cout << "a" << a << endl;
-				//dp[i] = max(a, dp[i]);
-
-			}
+		maxSum = max(maxSum, dp[i]);
+		if (i + T[i] > N + 1) {
+			continue;
 		}
-		P[i] = a + P[i];
+
+		dp[i + T[i]] = max(dp[i + T[i]], maxSum + P[i]);
+		////cout << dp[i + T[i]] << endl;
+		//dp[i + 1] = max(dp[i + 1], dp[i]);
 	}
 
-	for (int i = 1; i <= N; i++) {
-		//cout << P[i] << endl;
-		if (maxSum < P[i])
-			maxSum = P[i];
-	}
-
-	cout << maxSum;
+	cout << maxSum << endl;
 
 }
 
-//time over
+//i do work : i + T[i]
+//i do not work : i + 1
