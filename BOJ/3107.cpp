6d2b4e745e08eed0828bc507ededfbfece76a9f9
PARKNAYEON <parkna96@gmail.com>
2021-01-25 23:57:06 +0900

    1/25 3107
    success!!

--- BOJ/3107.cpp
+++ BOJ/3107.cpp
@@ -15,31 +15,54 @@ int main() {
 	//몇개의 그룹이 있는지
 	for (int i = 0; i < ip.size(); i++) {
 		if (ip[i] == ':') {
+			//g++;
+			if (ip[i + 1] == ':') {
+				i++;
+				continue;
+			}
 			g++;
 		}
 	}
-	
+	g++;
+
 	for (int i = 0; i < ip.size(); i++) {
 		count++;
 		if (ip[i] == ':') {
 			count--;
-			if (ip[i + 1] == ':') {
-				continue;
-			}
-			for (int j = 0; j < 4-count; j++) {
+
+			for (int j = 0; j < 4 - count; j++) {
 				result = result + '0';
 			}
 			for (int j = count; j > 0; j--) {
-				result = result + ip[i-j];
+				result = result + ip[i - j];
 			}
 			result = result + ':';
+
+			if (ip[i + 1] == ':') {
+				int zero = 8 - g - 1;
+				//cout << zero << endl;
+				if (i + 1 != ip.size() - 1) {
+					for (int i = 0; i < zero; i++) {
+						result = result + "0000:";
+					}
+				}
+				if (i + 1 == ip.size() - 1) {
+					for (int i = 0; i < zero; i++) {
+						result = result + "0000:";
+					}
+					result = result + "0000";
+				}
+				i++;
+				//continue;
+
+			}
 			count = 0;
 		}
 		if (ip[i] != ':' && i == ip.size() - 1) {
 			for (int j = 0; j < 4 - count; j++) {
 				result = result + '0';
 			}
-			for (int j = count-1; j >= 0; j--) {
+			for (int j = count - 1; j >= 0; j--) {
 				result = result + ip[i - j];
 			}
 		}
@@ -47,7 +70,7 @@ int main() {
 	//cout << g << endl;
 	cout << result;
 
-	
+
 
 
 }
