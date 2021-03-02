57b9b2621939fc12bd4de3c6376013da89bf4691
PARKNAYEON <parkna96@gmail.com>
2021-03-02 22:23:11 +0900

    3/2 타켓넘버

---
+++ programmers/타켓넘버.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+int answer = 0;
+
+
+using namespace std;
+
+
+void dfs(vector<int> numbers, int target, int i, int num) {
+    if (num == target && i == numbers.size()) {
+        answer++;
+        return;
+    }
+    if (i < numbers.size()) {
+        dfs(numbers, target, i + 1, num + numbers[i]);
+        dfs(numbers, target, i + 1, num - numbers[i]);
+    }
+}
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, 0);
+    return answer;
+}
